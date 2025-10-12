989bc1496f8d7b3b1914889be001aa61f948bacb
Tomas Peterec <info@tomaspeterec.sk>
2025-10-12 22:41:00 +0200

    Improved token handling

---
+++ src/store/myStore.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { create } from 'zustand'
+import { persist, createJSONStorage } from 'zustand/middleware'
+
+
+interface MyState {
+  myNumber: number | null
+  setMyNumber: (myNumber: number | null) => void
+}
+
+const isClient = typeof window !== 'undefined'
+
+export const useMyStore = create<MyState>()(
+  persist(
+    (set) => ({
+      myNumber: null,
+      setMyNumber: (myNumber) => set({ myNumber }),
+    }),
+    {
+      name: 'my-storage',
+      // only use storage on client
+      storage: isClient ? createJSONStorage(() => localStorage) : undefined,
+    }
+  )
+)
